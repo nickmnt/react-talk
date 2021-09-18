a14047926310f0a3566a0dc9a05cc88f56d76c55
nimamt <mtaghavinima@gmail.com>
2021-09-18 22:19:14 +0430

    Created a profile page.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,6 +15,7 @@ import { useStore } from '../stores/store';
 import { useEffect } from 'react';
 import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modals/ModalContainer';
+import ProfilePage from '../../features/profiles/ProfilePage';
 
 function App() {
   const location = useLocation();
@@ -47,9 +48,10 @@ function App() {
                 <Route path='/activities' component={ActivityDashboard} exact />
                 <Route path='/activities/:id' component={ActivityDetails} />
                 <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                <Route path='/login' component={LoginForm} />
+                <Route path='/profiles/:username' component={ProfilePage} />
                 <Route path='/errors' component={TestErrors} />
                 <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
