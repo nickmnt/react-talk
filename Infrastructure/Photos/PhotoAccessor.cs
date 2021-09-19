041fa937af5eea2ceb6f292362043f87c3e3d409
nimamt <mtaghavinima@gmail.com>
2021-09-19 17:14:37 +0430

    Small Fix.

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -5,12 +5,14 @@ using Application.Photos;
 using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Photos
 {
     public class PhotoAccessor : IPhotoAccessor
     {
+        private readonly ILogger _logger;
         private readonly Cloudinary _cloudinary;
 
         public PhotoAccessor(IOptions<CloudinarySettings> config)
@@ -18,7 +20,7 @@ namespace Infrastructure.Photos
             var account = new Account(
                 config.Value.CloudName,
                 config.Value.ApiKey,
-                config.Value.ApiKey);
+                config.Value.ApiSecret);
 
             _cloudinary = new Cloudinary(account);
         }
@@ -32,10 +34,12 @@ namespace Infrastructure.Photos
                     File = new FileDescription(file.FileName, stream),
                     Transformation = new Transformation().Height(500).Width(500).Crop("fill")
                 };
+                
                 var uploadResult = await _cloudinary.UploadAsync(uploadParams);
 
                 if (uploadResult.Error != null)
                 {
+                    Console.WriteLine(uploadResult.Error.Message);
                     throw new Exception(uploadResult.Error.Message);
                 }
 
