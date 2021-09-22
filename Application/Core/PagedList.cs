105885ebacae1608a2227f393182f255a2dda756
nimamt <mtaghavinima@gmail.com>
2021-09-22 16:29:07 +0330

    Fix.

--- Application/Core/PagedList.cs
+++ Application/Core/PagedList.cs
@@ -25,7 +25,7 @@ namespace Application.Core
         public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();
-            var items = await source.Skip((pageNumber - 1) * pageSize * pageSize)
+            var items = await source.Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize).ToListAsync();
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
