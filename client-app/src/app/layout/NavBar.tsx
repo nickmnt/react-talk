cfa2a336bd151998693c1ff4cb74f10819280180
nimamt <mtaghavinima@gmail.com>
2021-09-29 18:22:51 +0330

    Added a new chat icon

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { Button, Container, Dropdown, Image, Menu } from 'semantic-ui-react';
+import { Button, Container, Dropdown, Icon, Image, Menu } from 'semantic-ui-react';
 import { useStore } from '../stores/store';
 
 export default function NavBar() {
@@ -19,15 +19,20 @@ export default function NavBar() {
                 <Menu.Item>
                     <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
                 </Menu.Item>
-                <Menu.Item position='right'>
-                    <Image src={user?.image || '/assets/user.png'} avatar space='right'/>
-                    <Dropdown pointing='top left' text={user?.displayName}>
-                        <Dropdown.Menu>
-                          <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text='My Profile' icon='user' />
-                          <Dropdown.Item onClick={logout} text='Logout' icon='power' />    
-                        </Dropdown.Menu>           
-                    </Dropdown>
-                </Menu.Item>
+                <Menu.Menu position='right' >
+                    <Menu.Item>
+                        <Icon name='paper plane outline' size='large' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Image src={user?.image || '/assets/user.png'} avatar space='right' style={{marginRight: '.75rem'}}/>
+                        <Dropdown pointing='top right' text={user?.displayName} >
+                            <Dropdown.Menu>
+                            <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text='My Profile' icon='user' />
+                            <Dropdown.Item onClick={logout} text='Logout' icon='power' />    
+                            </Dropdown.Menu>           
+                        </Dropdown>
+                    </Menu.Item>
+                </Menu.Menu>
             </Container>
         </Menu>
     )
