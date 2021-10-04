2e25f25bc52e7049252589b0862f7777a099ec30
nimamt <mtaghavinima@gmail.com>
2021-10-04 12:27:17 +0330

    Removed placeholder.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -46,7 +46,6 @@ export default observer(function ActivityDashboard() {
       </Grid.Column>
       <Grid.Column width="6">
         <ActivityFilters />
-        <h2>Activity Filters</h2>
       </Grid.Column>
       <Grid.Column width="10">
         <Loader active={loadingNext} />
