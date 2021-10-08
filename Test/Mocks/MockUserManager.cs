8475ddcd7b1007a9a10f477e64d5acd8a46efed1
nimamt <mtaghavinima@gmail.com>
2021-10-08 16:04:21 +0330

    Added tests for application layer.

---
+++ Test/Mocks/MockUserManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace Test.Mocks
+{
+    public class MockUserManager
+    {
+        public static Mock<UserManager<TUser>> Create<TUser>(List<TUser> ls) where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+            mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+            mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
+            mgr.Setup(x => x.UpdateAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+            mgr.Setup(x => x.Users).Returns(ls.AsQueryable);
+            
+            return mgr;
+        }
+    }
+}
---
+++ Test/UnitTests/TestApplicationLayer/Activities/TestEdit.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Application.Activities;
+using Application.Core;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+using Test.Mocks;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestApplicationLayer.Activities
+{
+    public class TestEdit
+    {
+        [Fact]
+        public void EditUnknownActivity()
+        {
+            //Arrange
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                context.Database.EnsureCreated();
+                var users = new List<AppUser>();
+                Seed.SeedData(context, MockUserManager.Create(users).Object).Wait();
+                
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new MappingProfiles());
+                });
+                var mapper = config.CreateMapper();
+
+                var request = new Edit.Command { Activity = new Activity {Id = Guid.NewGuid()} }; 
+                var handler = new Edit.Handler(context, mapper);
+                
+                //Act
+                var result = handler.Handle(request, new System.Threading.CancellationToken()).Result;
+                
+                //Assert
+                result.ShouldBeNull();
+            }
+        }
+        
+        [Fact]
+        public void EditNormalActivity()
+        {
+            //Arrange
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                context.Database.EnsureCreated();
+                var users = new List<AppUser>();
+                Seed.SeedData(context, MockUserManager.Create(users).Object).Wait();
+                
+                var testActivity = new Activity
+                {
+                    Title = "Past Activity 1",
+                    Date = DateTime.Now.AddMonths(-2),
+                    Description = "Activity 2 months ago",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Pub",
+                    Attendees = new List<ActivityAttendee>
+                    {
+                        new ActivityAttendee
+                        {
+                            AppUser = new AppUser
+                            {
+                                DisplayName = "Bob",
+                                UserName = "bob",
+                                Email = "bob@test.com"
+                            },
+                            IsHost = true
+                        }
+                    }
+                };
+                var addedActivity = context.Add(testActivity);
+
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new MappingProfiles());
+                });
+                var mapper = config.CreateMapper();
+
+                var request = new Edit.Command { Activity = new Activity {Id=addedActivity.Entity.Id} }; 
+                var handler = new Edit.Handler(context, mapper);
+                
+                //Act
+                var result = handler.Handle(request, new System.Threading.CancellationToken()).Result;
+                
+                //Assert
+                result.ShouldNotBeNull();
+                result.IsSuccess.ShouldBeTrue();
+            }
+        }
+    }
+}
