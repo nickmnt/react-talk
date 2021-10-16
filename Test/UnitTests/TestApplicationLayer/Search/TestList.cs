d8d802f405f5ebade82258f16740fa8ed43f49c3
nimamt <mtaghavinima@gmail.com>
2021-10-16 23:22:32 +0330

    Added tests for normal search. (identical to chat search)

---
+++ Test/UnitTests/TestApplicationLayer/Search/TestList.cs
@@ -0,0 +1,117 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Search;
+using AutoMapper;
+using Domain;
+using Persistence;
+using Test.Mocks;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestApplicationLayer.Search
+{
+    public class TestList
+    {
+        [Fact]
+        public async Task TestFirstPartInTerm()
+        {
+            //Arrange
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                await context.SaveChangesAsync();
+                
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new MappingProfiles());
+                });
+                var mapper = config.CreateMapper();
+
+                var request = new List.Query {Term = "t"}; 
+                var handler = new List.Handler(context, mapper);
+                
+                //Act
+                var result = await handler.Handle(request, new CancellationToken());
+                
+                //Assert
+                result.IsSuccess.ShouldBeTrue();
+                result.Error.ShouldBeNull();
+                result.Value.Any(x => x.Username == "tom").ShouldBeTrue();
+            }
+        }
+        
+        [Fact]
+        public async Task TestMiddlePartInTerm()
+        {
+            //Arrange
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                await context.SaveChangesAsync();
+                
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new MappingProfiles());
+                });
+                var mapper = config.CreateMapper();
+
+                var request = new List.Query {Term = "t"}; 
+                var handler = new List.Handler(context, mapper);
+                
+                //Act
+                var result = await handler.Handle(request, new CancellationToken());
+                
+                //Assert
+                result.IsSuccess.ShouldBeTrue();
+                result.Error.ShouldBeNull();
+                result.Value.Any(x => x.Username == "tom").ShouldBeTrue();
+            }
+        }
+        
+        [Fact]
+        public async Task TestEndPartInTerm()
+        {
+            //Arrange
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                await context.SaveChangesAsync();
+                
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new MappingProfiles());
+                });
+                var mapper = config.CreateMapper();
+
+                var request = new List.Query {Term = "m"}; 
+                var handler = new List.Handler(context, mapper);
+                
+                //Act
+                var result = await handler.Handle(request, new CancellationToken());
+                
+                //Assert
+                result.IsSuccess.ShouldBeTrue();
+                result.Error.ShouldBeNull();
+                result.Value.Any(x => x.Username == "tom").ShouldBeTrue();
+            }
+        }
+    }
+}
