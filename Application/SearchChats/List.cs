ca60392604ce295fc800166e1a37d3c78f592f11
nimamt <mtaghavinima@gmail.com>
2021-10-16 22:55:00 +0330

    Search for chats, server side skeleton.

---
+++ Application/SearchChats/List.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Search;
+using MediatR;
+
+namespace Application.SearchChats
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<SearchResult>>>
+        {
+            
+        }
+        
+        public class Handler : IRequestHandler<Query, Result<List<SearchResult>>>
+        {
+            public async Task<Result<List<SearchResult>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                
+            }
+        }
+    }
+}
