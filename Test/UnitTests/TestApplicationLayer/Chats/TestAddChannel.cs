5173eef859859e9029b637f1b960151b80f34c7d
nimamt <mtaghavinima@gmail.com>
2021-12-04 21:31:33 +0330

    Updated test

--- Test/UnitTests/TestApplicationLayer/Chats/TestAddChannel.cs
+++ Test/UnitTests/TestApplicationLayer/Chats/TestAddChannel.cs
@@ -1,7 +1,55 @@
-﻿namespace Test.UnitTests.TestApplicationLayer.Chats
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Chats.ChannelChats;
+using Application.Core;
+using AutoMapper;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Test.Mocks;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestApplicationLayer.Chats
 {
     public class TestAddChannel
     {
-        
+        [Fact]
+        public async Task TestFlawless()
+        {
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfiles()); });
+                var mapper = config.CreateMapper();
+
+                var request = new Create.Command { Name = "channel", Description = "optional" };
+                var userAccessor = MockUserAccessor.Create().Object;
+                var handler = new Create.Handler(context, mapper, userAccessor);
+
+                //Act
+                var result = await handler.Handle(request, new System.Threading.CancellationToken());
+
+                var tomChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking().FirstOrDefaultAsync(x => x.AppUser.UserName == "tom");
+                var bobChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.AppUser.UserName == userAccessor.GetUsername());
+
+                //Assert
+                result.Value.ShouldNotBeNull();
+                tomChat.ShouldNotBeNull();
+                bobChat.ShouldNotBeNull();
+                Assert.Equal(result.Value.Id.ToString(), bobChat.Chat.Id.ToString());
+                Assert.Equal(result.Value.Id.ToString(), tomChat.Chat.Id.ToString());
+            }
+        }
     }
 }
