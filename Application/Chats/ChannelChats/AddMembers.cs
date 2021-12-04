7c9c3a339ef3676a54ad292102a27c2243ad1fa7
nimamt <mtaghavinima@gmail.com>
2021-12-04 22:43:36 +0330

    Added missing ctor

--- Application/Chats/ChannelChats/AddMembers.cs
+++ Application/Chats/ChannelChats/AddMembers.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
 using FluentValidation;
 using MediatR;
+using Persistence;
 
 namespace Application.Chats.ChannelChats
 {
-    public class AddMember
+    public class AddMembers
     {
         public class Command : IRequest<Result<bool>>
         {
@@ -26,6 +29,16 @@ namespace Application.Chats.ChannelChats
 
         public class Handler : IRequestHandler<Command, Result<bool>>
         {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            private readonly IUserAccessor _accessor;
+
+            public Handler(DataContext context, IMapper mapper, IUserAccessor accessor)
+            {
+                _context = context;
+                _mapper = mapper;
+                _accessor = accessor;
+            }
             public async Task<Result<bool>> Handle(Command request, CancellationToken cancellationToken)
             {
                 
