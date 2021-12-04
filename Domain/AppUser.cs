3827931b328918f0b6f598ecb58fb30e9b50d649
nimamt <mtaghavinima@gmail.com>
2021-12-04 21:02:57 +0330

    Updated db schema

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -8,15 +8,15 @@ namespace Domain
     {
         public string DisplayName { get; set; }
         public string Bio { get; set; }
-        public ICollection<ActivityAttendee> Activities { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-        public ICollection<UserFollowing> Followings { get; set; }
-        public ICollection<UserFollowing> Followers { get; set; }
+        public ICollection<ActivityAttendee> Activities { get; set; } = new List<ActivityAttendee>();
+        public ICollection<Photo> Photos { get; set; } = new List<Photo>();
+        public ICollection<UserFollowing> Followings { get; set; } = new List<UserFollowing>();
+        public ICollection<UserFollowing> Followers { get; set; } = new List<UserFollowing>();
         public ICollection<CommentNotification> CommentNotifications { get; set; } = new List<CommentNotification>();
         public ICollection<FollowNotification> FollowNotifications { get; set; } = new List<FollowNotification>();
         public ICollection<JoinNotification> JoinNotifications { get; set; } = new List<JoinNotification>();
-        public ICollection<UserChat> Chats { get; set; }
-        public ICollection<ChannelMembership> Channels { get; set; }
-        public ICollection<GroupMembership> Groups { get; set; }
+        public ICollection<UserChat> Chats { get; set; } = new List<UserChat>();
+        public ICollection<ChannelMembership> Channels { get; set; } = new List<ChannelMembership>();
+        public ICollection<GroupMembership> Groups { get; set; } = new List<GroupMembership>();
     }
 }
