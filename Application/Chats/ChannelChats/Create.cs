70511e5deb0ba534fadc487a7a896fdb8879b1d3
nimamt <mtaghavinima@gmail.com>
2021-12-04 21:34:23 +0330

    Updated channel creation code, fixed validation

--- Application/Chats/ChannelChats/Create.cs
+++ Application/Chats/ChannelChats/Create.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
 using Application.Interfaces;
 using AutoMapper;
+using Domain.Direct;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Chats.ChannelChats
@@ -22,7 +25,6 @@ namespace Application.Chats.ChannelChats
             public CommandValidator()
             {
                 RuleFor(x => x.Name).NotEmpty();
-                RuleFor(x => x.Description).NotEmpty();
             }
         }
 
@@ -41,7 +43,20 @@ namespace Application.Chats.ChannelChats
             
             public async Task<Result<ChatDto>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var channelChat = new ChannelChat();
+                var chat = new Chat { Type = ChatType.Channel, ChannelChat = channelChat };
+
+                var user = await _context.Users
+                    .SingleOrDefaultAsync(x => x.UserName == _accessor.GetUsername(), cancellationToken);
+
+                var userChat = new UserChat { Chat = chat, AppUser = user };
+                _context.UserChats.Add(userChat);
                 
+                var result = await _context.SaveChangesAsync(cancellationToken);
+                if (result > 0)
+                    return Result<ChatDto>.Success(_mapper.Map<UserChat,ChatDto>(userChat));
+                
+                return Result<ChatDto>.Failure("Failed to create the new channel.");
             }
         }
     }
