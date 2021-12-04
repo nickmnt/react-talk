f14b9a939051eded19617958e453bf6646fdacaf
nimamt <mtaghavinima@gmail.com>
2021-12-04 18:57:58 +0330

    Create group, application layer and tests, initial

---
+++ Application/Chats/GroupChats/Create.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Direct;
+using FluentValidation;
+using MediatR;
+using Persistence;
+
+namespace Application.Chats.GroupChats
+{
+    public class Create
+    {
+        public class Command : IRequest<Result<ChatDto>>
+        {
+            public string Name { get; set; }
+            public List<string> Members { get; set; }
+        }
+        
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Name).NotEmpty();
+                RuleFor(x => x.Members).NotEmpty();
+            }
+        }
+        
+        public class Handler : IRequestHandler<Command, Result<ChatDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            private readonly IUserAccessor _accessor;
+
+            public Handler(DataContext context, IMapper mapper, IUserAccessor accessor)
+            {
+                _context = context;
+                _mapper = mapper;
+                _accessor = accessor;
+            }
+            
+            public Task<Result<ChatDto>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var groupChat = new GroupChat();
+                var chat = new Chat {Type = }
+            }
+        }
+    }
+}
---
+++ Test/UnitTests/TestApplicationLayer/Chats/TestAddGroup.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Chats.GroupChats;
+using Application.Core;
+using AutoMapper;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Test.Mocks;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestApplicationLayer.Chats
+{
+    public class TestAddGroup
+    {
+        [Fact]
+        public async Task TestEmptyName()
+        {
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfiles()); });
+                var mapper = config.CreateMapper();
+
+                var request = new Create.Command { Name = "", Members = { "tom" } };
+                var userAccessor = MockUserAccessor.Create().Object;
+                var handler = new Create.Handler(context, mapper, userAccessor);
+
+                //Act
+                var result = await handler.Handle(request, new System.Threading.CancellationToken());
+
+                var tomChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking().FirstOrDefaultAsync(x => x.AppUser.UserName == "tom");
+                var bobChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.AppUser.UserName == userAccessor.GetUsername());
+
+                //Assert
+                result.ShouldBeNull();
+                tomChat.ShouldBeNull();
+                bobChat.ShouldBeNull();
+            }
+        }
+
+        [Fact]
+        public async Task TestEmptyMembers()
+        {
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfiles()); });
+                var mapper = config.CreateMapper();
+
+                var request = new Create.Command { Name = "group", Members = { "" } };
+                var userAccessor = MockUserAccessor.Create().Object;
+                var handler = new Create.Handler(context, mapper, userAccessor);
+
+                //Act
+                var result = await handler.Handle(request, new System.Threading.CancellationToken());
+
+                var tomChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking().FirstOrDefaultAsync(x => x.AppUser.UserName == "tom");
+                var bobChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.AppUser.UserName == userAccessor.GetUsername());
+
+                //Assert
+                result.ShouldBeNull();
+                tomChat.ShouldBeNull();
+                bobChat.ShouldBeNull();
+            }
+        }
+
+        [Fact]
+        public async Task TestFakeMembers()
+        {
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfiles()); });
+                var mapper = config.CreateMapper();
+
+                var request = new Create.Command { Name = "group", Members = { "tom", "kfkf" } };
+                var userAccessor = MockUserAccessor.Create().Object;
+                var handler = new Create.Handler(context, mapper, userAccessor);
+
+                //Act
+                var result = await handler.Handle(request, new System.Threading.CancellationToken());
+
+                var tomChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking().FirstOrDefaultAsync(x => x.AppUser.UserName == "tom");
+                var bobChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.AppUser.UserName == userAccessor.GetUsername());
+
+                //Assert
+                result.ShouldNotBeNull();
+                tomChat.ShouldNotBeNull();
+                bobChat.ShouldNotBeNull();
+                Assert.Equal(result.Value.Id.ToString(), bobChat.Chat.Id.ToString());
+                Assert.Equal(result.Value.Id.ToString(), tomChat.Chat.Id.ToString());
+            }
+        }
+
+        [Fact]
+        public async Task TestManyMembers()
+        {
+            var options = SqliteInMemory.CreateOptions<DataContext>();
+            using (var context = new DataContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+                var users = new List<AppUser>();
+                await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfiles()); });
+                var mapper = config.CreateMapper();
+
+                var request = new Create.Command { Name = "group", Members = { "tom", "tom" } };
+                var userAccessor = MockUserAccessor.Create().Object;
+                var handler = new Create.Handler(context, mapper, userAccessor);
+
+                //Act
+                var result = await handler.Handle(request, new System.Threading.CancellationToken());
+
+                var tomChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking().FirstOrDefaultAsync(x => x.AppUser.UserName == "tom");
+                var bobChat = await context.UserChats
+                    .Include(x => x.Chat)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.AppUser.UserName == userAccessor.GetUsername());
+
+                //Assert
+                result.ShouldNotBeNull();
+                tomChat.ShouldNotBeNull();
+                bobChat.ShouldNotBeNull();
+                Assert.Equal(result.Value.Id.ToString(), bobChat.Chat.Id.ToString());
+                Assert.Equal(result.Value.Id.ToString(), tomChat.Chat.Id.ToString());
+            }
+        }
+
+        [Fact]
+            public async Task TestFlawless()
+            {
+                var options = SqliteInMemory.CreateOptions<DataContext>();
+                using (var context = new DataContext(options))
+                {
+                    await context.Database.EnsureCreatedAsync();
+                    var users = new List<AppUser>();
+                    await Seed.SeedData(context, MockUserManager.Create(users).Object);
+
+                    var config = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfiles()); });
+                    var mapper = config.CreateMapper();
+
+                    var request = new Create.Command { Name = "group", Members = { "tom" } };
+                    var userAccessor = MockUserAccessor.Create().Object;
+                    var handler = new Create.Handler(context, mapper, userAccessor);
+
+                    //Act
+                    var result = await handler.Handle(request, new System.Threading.CancellationToken());
+
+                    var tomChat = await context.UserChats
+                        .Include(x => x.Chat)
+                        .AsNoTracking().FirstOrDefaultAsync(x => x.AppUser.UserName == "tom");
+                    var bobChat = await context.UserChats
+                        .Include(x => x.Chat)
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.AppUser.UserName == userAccessor.GetUsername());
+
+                    //Assert
+                    result.ShouldNotBeNull();
+                    tomChat.ShouldNotBeNull();
+                    bobChat.ShouldNotBeNull();
+                    Assert.Equal(result.Value.Id.ToString(), bobChat.Chat.Id.ToString());
+                    Assert.Equal(result.Value.Id.ToString(), tomChat.Chat.Id.ToString());
+                }
+            }
+        }
+}
