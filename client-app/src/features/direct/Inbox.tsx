f66c23c352e6b1026b5edd15b98aa5b7e762009f
nimamt <mtaghavinima@gmail.com>
2021-12-04 18:58:30 +0330

    Clean up

--- client-app/src/features/direct/Inbox.tsx
+++ client-app/src/features/direct/Inbox.tsx
@@ -1,7 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react'
-import { Grid } from 'semantic-ui-react';
-import NavBar from '../../app/layout/NavBar';
+import { useEffect } from 'react'
 import { useStore } from '../../app/stores/store';
 import ChatView from './chat-view/Index'
 import HomeSidebar from './HomeSidebar'
