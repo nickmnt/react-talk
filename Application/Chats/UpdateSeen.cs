564c291df7f8e14c29026bf43dc87c700484650d
nimamt <mtaghavinima@gmail.com>
2021-12-26 23:15:01 +0330

    Added seen to db, created classes for membership

---
+++ Application/Chats/UpdateSeen.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Chats
+{
+    public class UpdateSeen
+    {
+        
+    }
+}
---
+++ Domain/Direct/GroupMember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Domain.Direct
+{
+    public class GroupMembership
+    {
+        public int MemberType { get; set; }
+        public string DisplayName { get; set; }
+        public string Username { get; set; }
+        public string Image { get; set; }
+        public DateTime LastSeen { get; set; }
+        [JsonIgnore] public Guid ChatId { get; set; }
+    }
+}
