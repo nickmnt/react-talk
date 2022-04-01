68dfd3b4fbaaaea1fa99c569bf12c200d369c637
Devin <mtaghavinima@gmail.com>
2022-04-02 00:53:29 +0430

    Better chat input UI

--- client-app/src/features/direct/chat-view/ChatTextInput.tsx
+++ client-app/src/features/direct/chat-view/ChatTextInput.tsx
@@ -37,7 +37,7 @@ export default observer(function ChatTextInput({ props, isValid, handleSubmit }:
 
     return (
         <TextareaAutosize
-            maxRows={2}
+            maxRows={5}
             className="chatInput__input"
             placeholder="Message"
             {...props.field}
